661427cb29b1636480294f965daf9c432ade1799
Iman Lzr <imanelaazraoui@gmail.com>
2024-10-11 18:11:21 +0200

    creat splashscreen,signIn

---
+++ app/src/main/java/com/example/todoapp/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
